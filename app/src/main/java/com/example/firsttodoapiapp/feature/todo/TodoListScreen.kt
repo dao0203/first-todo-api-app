17a225632288f562ac889dc141e5f1c5667b4f72
dao0203 <youzaizuoteng872@gmail.com>
2024-01-05 23:00:38 +0900

    add TodoListScreen

---
+++ app/src/main/java/com/example/firsttodoapiapp/feature/todo/TodoListScreen.kt
@@ -0,0 +1,10 @@
+package com.example.firsttodoapiapp.feature.todo
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun TodoListScreen(
+    viewModel: TodoListViewModel = hiltViewModel()
+) {
+}
---
+++ app/src/main/java/com/example/firsttodoapiapp/feature/todo/TodoListViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.firsttodoapiapp.feature.todo
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TodoListViewModel @Inject constructor() : ViewModel() {
+}
